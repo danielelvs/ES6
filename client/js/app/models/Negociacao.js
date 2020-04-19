8604e8f88edf2d3ef7fe396e25c203a494f98ac9
Daniele Leite <danii.exe@gmail.com>
2020-04-19 18:47:31 -0300

    ES6 I - Aula 2 - Pt 4

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,17 +1,29 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
+    getData() {
+        return this._data;
+    }
+    
+    getQuantidade() {
+        return this._quantidade;
+    }
 
-    obtemVolume() {
-        return this.quantidade * this.valor;
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
     }
 
 }
 
 
-// dentro de classe metodo, fora de classe funcao
+// dentro de classe metodo, fora de classe funcao.
+// _(underline) convensao para o programador que esses atributos so podem acessa-los pela propria classe e nao fora.
