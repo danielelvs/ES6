23e78e5decb3534abb020a2492177c5ce74a3cb0
Daniele Leite <danii.exe@gmail.com>
2020-04-21 15:11:30 -0300

    ES6 I - Aula 3 - Pt 3

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,19 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document); //criando alias de document query selector
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault(); // cancela a submissao do formulario
-        
-
-        let $ = document.querySelector.bind(document); //criando alias de document query selector
-
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
-
-        console.log(inputData.value, inputQuantidade.value, inputValor.value);
+        console.log(
+            this.inputData.value, 
+            this.inputQuantidade.value, 
+            this.inputValor.value
+        );
     }
 
 }
